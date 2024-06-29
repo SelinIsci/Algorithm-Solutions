0aa06111ed81a1130689a2449232baf374233791
Selin <isci.selin@gmail.com>
2024-06-29 10:46:02 +0300

    Return Negative

---
+++ CodeWars/Java/8 kyu/Return_Negative.java
@@ -0,0 +1,13 @@
+public class Kata {
+
+  public static int makeNegative(final int x) {
+    if(x<0) 
+    return x;
+    else if(x>0)
+    return x * (-1);
+    else return 0;
+    
+    
+  }
+  
+}
