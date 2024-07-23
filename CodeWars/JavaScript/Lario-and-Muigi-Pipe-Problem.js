0c692b36ed6f79283351a491e823bbc3c2fea54c
Selin <isci.selin@gmail.com>
2024-07-23 15:31:21 +0300

    changed direction

--- CodeWars/JavaScript/Lario-and-Muigi-Pipe-Problem.js
+++
@@ -1,6 +0,0 @@
-function pipeFix(numbers) {
-  return Array.from(
-    { length: Math.max(...numbers) + 1 - Math.min(...numbers) },
-    (_, i) => i + Math.min(...numbers)
-  );
-}
--- CodeWars/JavaScript/Sort-and-Star.js
+++
@@ -1,6 +0,0 @@
-function twoSort(s) {
-  s.sort();
-  const firstValue = s[0];
-  const formattedString = firstValue.split('').join('***');
-  return formattedString;
-}
