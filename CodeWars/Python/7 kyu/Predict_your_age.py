db89817434ae861130a98e472f627c5ec00e9f2c
Selin <isci.selin@gmail.com>
2025-07-01 22:54:57 +0300

    Predict your age!

---
+++ CodeWars/Python/7 kyu/Predict_your_age.py
@@ -0,0 +1,16 @@
+import math
+
+
+def predict_age(age_1, age_2, age_3, age_4, age_5, age_6, age_7, age_8):
+    total = (
+        age_1**2
+        + age_2**2
+        + age_3**2
+        + age_4**2
+        + age_5**2
+        + age_6**2
+        + age_7**2
+        + age_8**2
+    )
+
+    return math.floor(math.sqrt(total) / 2)
