79e8a2e3092e7dc75e0f062cf1544963793b4ad8
Selin <isci.selin@gmail.com>
2025-07-24 09:27:51 +0300

    P-0020

---
+++ LeetCode/Python/0020_Valid_Parentheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        bracket_map = {")": "(", "}": "{", "]": "["}
+
+        for c in s:
+            if c in "([{":
+                stack.append(c)
+            elif c in ")]}":
+                if not stack or stack[-1] != bracket_map[c]:
+                    return False
+                stack.pop()
+        return not stack
